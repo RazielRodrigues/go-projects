48af79fce692810198f7c402269babfbcb5e6dff
Raziel Rodrigues <razielx3@live.com>
2024-09-01 14:08:57 +0100

    finalizando parte das funcoes

--- coder/array/arrays.go
+++ coder/array/arrays.go
@@ -3,11 +3,15 @@ package main
 import "fmt"
 
 func main() {
-	notas()
-	pedacoes()
+	arrays()
+	ranges()
+	pedacos()
+	makerzao()
+	mapeando()
+
 }
 
-func notas() {
+func arrays() {
 	var notas [3]float64
 	notas[0], notas[1], notas[2] = 1.1, 4.5, 9.0
 
@@ -21,18 +25,89 @@ func notas() {
 	fmt.Println("Media: ", media)
 }
 
-func pedacoes() {
+func ranges() {
 
-	var numeros = [...]int{1, 2, 3, 4, 5}
+	var numeros = [...]int{1, 2, 3, 4, 5, 7, 8, 9}
 
 	// EQUIVALENTE FOR EACH
 	for index, valor := range numeros {
-		println(index, valor)
+		fmt.Println(index, valor)
 	}
 	for index, _ := range numeros {
-		println(index)
+		fmt.Println(index)
 	}
 	for sopegaoindex := range numeros {
-		println(sopegaoindex)
+		fmt.Println(sopegaoindex)
 	}
+}
+
+func pedacos() {
+	var numeros = [10]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	var fixos = [...]string{"a", "b", "c"}
+
+	var pedacoes = []int{4, 5, 6}
+
+	fmt.Println(numeros, fixos, pedacoes)
+
+	pedacoFixo := numeros[5:9]
+	pedacoDoInicio := numeros[:4]
+	pedacoDoPedaco := pedacoFixo[1:2]
+
+	fmt.Println(pedacoFixo, pedacoDoInicio, pedacoDoPedaco)
+}
+
+func makerzao() {
+	pedaco := make([]int, 10) // o array criado é de 10 assim como o tamanho do slice
+
+	pedacoDoPedaco := make([]int, 5, 30) // o slice criado tem o tamanho de cinco e o tamanho do array é 30
+
+	pedacoMenor := make([]int, 0, 10)
+	fmt.Println(pedacoMenor, len(pedacoMenor), cap(pedacoMenor))
+
+	pedacoMenor = append(pedacoMenor, 10, 10, 10)
+
+	fmt.Println(pedaco, pedacoDoPedaco, pedacoMenor)
+
+	var copia = make([]int, 2)
+
+	copy(copia, pedacoMenor)
+	fmt.Println(copia)
+}
+
+func mapeando() {
+	aprovados := make(map[int]string)
+
+	aprovados[1] = "Raziel"
+	aprovados[2] = "Jaiara"
+	aprovados[3] = "Sergio"
+	aprovados[4] = "Marcos"
+	// mesma chave sobrescreve
+	aprovados[4] = "Lisa"
+	fmt.Println(aprovados)
+	for chave, nome := range aprovados {
+		fmt.Println(chave, nome)
+	}
+
+	delete(aprovados, 1)
+	fmt.Println(aprovados)
+
+	dados := map[string]string{
+		"nome":    "ai sim",
+		"entendi": "eu tambem",
+	}
+
+	fmt.Println(dados)
+
+	muitosDados := map[int]map[string]int{
+		0: {
+			"OLA": 1000,
+		},
+		1: {
+			"que coisa feia!": 2000,
+		},
+	}
+
+	// da pra deletar e fazer um for dentro do for com essa estrutura
+	fmt.Println(muitosDados)
+
 }
--- coder/funcoes/funcoes.go
+++ coder/funcoes/funcoes.go
@@ -10,7 +10,110 @@ func imprimir(valor int) {
 	fmt.Println(valor)
 }
 
+// pode usar retorno limpo pq ja declarou em cima na parte do retorno
+// as variaveis do retorno tambem sao acessiveis dentro dele
+// cada variavel pode ter um tipo diferente
+// podemos tamvem fazer como a forma minimizada
+func retornoNomeado(p1, p2 int) (segundo int, primeiro int, nome string) {
+	segundo = p2
+	primeiro = p1
+	nome = "aaa"
+
+	return // retorno limpo
+}
+
+var soma = func(a, b int) {
+	fmt.Println("funcao dentro de variavel")
+}
+
+func vai(a string) {
+	fmt.Println(a)
+}
+
+func agora(funcao func(string)) {
+	funcao("TA VENDO, FOI!")
+}
+
+func comoassim(chamandobb func(string), teste string) {
+	chamandobb("IDENTIFICOU QUE A FUNÇÃO QUE EU TINHA PASSADO NA TINHA RETORNO HUUUUUUUUUUUUUUM TA INTELIGENTE EM!")
+	fmt.Println(teste)
+}
+
+func variariandoTipoBrutos(varios ...int) {
+	for _, v := range varios {
+		fmt.Println(v)
+	}
+}
+
+func variandoUmSlicePoisEcomoDesctructing(pedaco ...string) {
+	for _, v := range pedaco {
+		fmt.Println(v)
+	}
+}
+
 func main() {
+
+	// basico funcao e metodos com e sem retorno
 	resultado := somar(5, 4)
 	imprimir(resultado)
+
+	// multiplo retorno
+	segundo, primeiro, _ := retornoNomeado(1, 2)
+	fmt.Println(segundo, primeiro)
+
+	// funcao dentro de variavel
+	soma(1, 2)
+
+	// funcao dentro de variavel em escopo local
+	queijo := func() {
+		fmt.Println("queijo!")
+	}
+	queijo()
+
+	// passando funcao pra funcao
+	agora(vai)
+	comoassim(vai, "chama")
+
+	// argumentos variaveis
+	variariandoTipoBrutos(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+	pedacao := []string{"Raziel", "Jaiara"}
+	variandoUmSlicePoisEcomoDesctructing(pedacao...)
+
+	// multiplica o resultado da multiplicaçºao atual com o resultado da proxima menos o numero atual
+	fmt.Println(recursividadeFatorial(3))
+	fmt.Println(recursividadeFatorial(4))
+	fmt.Println(recursividadeFatorial(5))
+	fmt.Println(recursividadeFatorial(6))
+
+	atrasandoOutraFuncao()
+}
+
+func closure() func() {
+
+	mostra := func() {
+		fmt.Println("fechado!")
+	}
+	return mostra
+}
+
+func recursividadeFatorial(numero uint) uint {
+	switch {
+	case numero == 0:
+		return 1
+	default:
+		resultado := numero * recursividadeFatorial(numero-1)
+		return resultado
+	}
+}
+
+func atrasandoOutraFuncao() {
+	defer fmt.Println("Chamando atrasado")
+
+	for i := 0; i < 3; i++ {
+		fmt.Println(i)
+	}
+}
+
+func init() {
+	fmt.Println("Aqui a gente consegue iniciar algo para o pacote sera sempre a primeira funcao buscada, esse voce pode ter varios no projeto mais de um ja a funcao main é uma por projeto")
 }
