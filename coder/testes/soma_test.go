1c7b0c7a1f4e771d10653cff24545e83b5691ca8
Raziel Rodrigues <razielx3@live.com>
2024-10-26 17:27:50 +0100

    go tests

---
+++ coder/testes/soma_test.go
@@ -0,0 +1,24 @@
+package soma
+
+import "testing"
+
+func TestSoma(t *testing.T) {
+	t.Parallel()
+	dataset := []struct {
+		a        int
+		b        int
+		expected int
+	}{
+		{2, 3, 5},
+		{0, 0, 0},
+		{-1, 1, 0},
+	}
+
+	for _, data := range dataset {
+		result := Soma(data.a, data.b)
+		if result != data.expected {
+			t.Errorf("Soma(%d, %d) = %d, want %d", data.a, data.b, result, data.expected)
+		}
+	}
+
+}
